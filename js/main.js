833b765f5300daba5d994cb1fa369696a97a7fed
DenY <denys.yakym@gmail.com>
2019-04-12 21:18:40 +0300

    Start create JS logic

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const container = document.getElementById('container');
+const main = document.getElementById('main');
+
+class Timer {
+	constructor(setMinutes, setSeconds, start, timeout) {
+		this.setMinutes = setMinutes;
+		this.setSeconds = setSeconds;
+		this.start = start;
+		this.timeout = timeout;
+		this.render();
+	}
+}
+
+createTimer() {
+ 	this.timer
+}
